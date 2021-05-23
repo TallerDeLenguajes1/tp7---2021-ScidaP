77a837f527b0f0918e5ffe081743cfc86ad5de59
Patricio Scidá <patricioscida@gmail.com>
2021-05-23 15:14:44 -0300

    Mejorados los onclick

--- Tp6Calculadora/Form1.Designer.cs
+++ Tp6Calculadora/Form1.Designer.cs
@@ -73,7 +73,7 @@ namespace Tp6Calculadora {
             this.button1.TabIndex = 3;
             this.button1.Text = "1";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.button_Click);
             // 
             // button2
             // 
@@ -86,7 +86,7 @@ namespace Tp6Calculadora {
             this.button2.TabIndex = 4;
             this.button2.Text = "2";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.button_Click);
             // 
             // button3
             // 
@@ -96,7 +96,7 @@ namespace Tp6Calculadora {
             this.button3.TabIndex = 5;
             this.button3.Text = "3";
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.Click += new System.EventHandler(this.button_Click);
             // 
             // button4
             // 
@@ -107,7 +107,7 @@ namespace Tp6Calculadora {
             this.button4.TabIndex = 6;
             this.button4.Text = "4";
             this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.Click += new System.EventHandler(this.button_Click);
             // 
             // button5
             // 
@@ -117,7 +117,7 @@ namespace Tp6Calculadora {
             this.button5.TabIndex = 7;
             this.button5.Text = "5";
             this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
+            this.button5.Click += new System.EventHandler(this.button_Click);
             // 
             // button7
             // 
@@ -128,7 +128,7 @@ namespace Tp6Calculadora {
             this.button7.TabIndex = 9;
             this.button7.Text = "7";
             this.button7.UseVisualStyleBackColor = true;
-            this.button7.Click += new System.EventHandler(this.button7_Click);
+            this.button7.Click += new System.EventHandler(this.button_Click);
             // 
             // button8
             // 
@@ -138,7 +138,7 @@ namespace Tp6Calculadora {
             this.button8.TabIndex = 10;
             this.button8.Text = "8";
             this.button8.UseVisualStyleBackColor = true;
-            this.button8.Click += new System.EventHandler(this.button8_Click);
+            this.button8.Click += new System.EventHandler(this.button_Click);
             // 
             // button9
             // 
@@ -148,7 +148,7 @@ namespace Tp6Calculadora {
             this.button9.TabIndex = 11;
             this.button9.Text = "9";
             this.button9.UseVisualStyleBackColor = true;
-            this.button9.Click += new System.EventHandler(this.button9_Click);
+            this.button9.Click += new System.EventHandler(this.button_Click);
             // 
             // textBox1
             // 
@@ -168,7 +168,7 @@ namespace Tp6Calculadora {
             this.button10.TabIndex = 13;
             this.button10.Text = "+";
             this.button10.UseVisualStyleBackColor = true;
-            this.button10.Click += new System.EventHandler(this.button10_Click);
+            this.button10.Click += new System.EventHandler(this.button_Click);
             // 
             // button11
             // 
@@ -179,7 +179,7 @@ namespace Tp6Calculadora {
             this.button11.TabIndex = 14;
             this.button11.Text = "-";
             this.button11.UseVisualStyleBackColor = true;
-            this.button11.Click += new System.EventHandler(this.button11_Click);
+            this.button11.Click += new System.EventHandler(this.button_Click);
             // 
             // button12
             // 
@@ -190,7 +190,7 @@ namespace Tp6Calculadora {
             this.button12.TabIndex = 15;
             this.button12.Text = "*";
             this.button12.UseVisualStyleBackColor = true;
-            this.button12.Click += new System.EventHandler(this.button12_Click);
+            this.button12.Click += new System.EventHandler(this.button_Click);
             // 
             // button13
             // 
@@ -201,7 +201,7 @@ namespace Tp6Calculadora {
             this.button13.TabIndex = 16;
             this.button13.Text = "/";
             this.button13.UseVisualStyleBackColor = true;
-            this.button13.Click += new System.EventHandler(this.button13_Click);
+            this.button13.Click += new System.EventHandler(this.button_Click);
             // 
             // button14
             // 
@@ -223,7 +223,7 @@ namespace Tp6Calculadora {
             this.button6.TabIndex = 18;
             this.button6.Text = "6";
             this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
+            this.button6.Click += new System.EventHandler(this.button_Click);
             // 
             // label3
             // 
--- Tp6Calculadora/Form1.cs
+++ Tp6Calculadora/Form1.cs
@@ -18,68 +18,8 @@ namespace Tp6Calculadora {
 
         }
 
-        private void button1_Click(object sender, EventArgs e) {
-            textBox1.Text += "1";
-        }
-
         private void label4_Click(object sender, EventArgs e) {
 
-        }
-
-        private void button2_Click(object sender, EventArgs e) {
-            textBox1.Text += "2";
-        }
-
-        private void button3_Click(object sender, EventArgs e) {
-            textBox1.Text += "3";
-        }
-
-        private void button4_Click(object sender, EventArgs e) {
-            textBox1.Text += "4";
-        }
-
-        private void button5_Click(object sender, EventArgs e) {
-            textBox1.Text += "5";
-        }
-
-        private void button6_Click(object sender, EventArgs e) {
-            textBox1.Text += "6";
-        }
-
-        private void button7_Click(object sender, EventArgs e) {
-            textBox1.Text += "7";
-        }
-
-        private void button8_Click(object sender, EventArgs e) {
-            textBox1.Text += "8";
-        }
-
-        private void button9_Click(object sender, EventArgs e) {
-            textBox1.Text += "9";
-        }
-
-        private void button10_Click(object sender, EventArgs e) {
-            if (textBox1.Text != "") {
-                textBox1.Text += " + ";
-            }
-        }
-
-        private void button11_Click(object sender, EventArgs e) {
-            if (textBox1.Text != "") {
-                textBox1.Text += " - ";
-            }
-        }
-
-        private void button12_Click(object sender, EventArgs e) {
-            if (textBox1.Text != "") {
-                textBox1.Text += " * ";
-            }
-        }
-
-        private void button13_Click(object sender, EventArgs e) {
-            if (textBox1.Text != "") {
-                textBox1.Text += " / ";
-            }
         }
 
         private void button14_Click(object sender, EventArgs e) {
@@ -87,8 +27,12 @@ namespace Tp6Calculadora {
         }
 
         private void button15_Click(object sender, EventArgs e) { // Botón de igual
-            string operacion = textBox1.Text;
-            for (int i = 0; i < operacion.Length; i)
+
+        }
+
+        private void button_Click(object sender, EventArgs e) {
+            Button boton = (Button)sender;
+            textBox1.Text += boton.Text; // Me parecía muy feo el código que había escrito, era obvio que había una forma de escribirlo mejor xd
         }
     }
 }
